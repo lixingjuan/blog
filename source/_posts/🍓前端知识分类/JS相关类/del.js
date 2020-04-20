a7c1cae0ae488d20ffc67f342a3b17a90994d3c8
25037 <xingjuan.li@hand-china.com>
2020-04-20 22:47:57 +0800

    add:docs:继承相关

--- source/_posts/🍓前端知识分类/JS相关类/del.js
+++ source/_posts/🍓前端知识分类/JS相关类/del.js
@@ -1,15 +1,33 @@
-function Father() {
+/* 借用构造函数实现继承 */
+function Father(name) {
+  this.name = name;
   this.colors = ["red", "green"];
-  console.log(this.colors);
 }
-function Son() {}
-Son.prototype = new Father(); // 使Son的原型指向Father的实例
-const instance1 = new Son();
-const instance2 = new Son();
 
-console.log(Father.prototype.isPrototypeOf(instance1));
-console.log(Father.prototype.isPrototypeOf(instance2));
-instance1.colors.push("black");
-console.log(instance1.colors); // [ 'red', 'green', 'black' ]
+Father.prototype.sayName = function() {
+  console.log(this.name);
+};
+
+function Son(name, age) {
+  Father.call(this, name);
+  this.age = age;
+}
+
+// 继承方法
+Son.prototype = new Father();
+Son.prototype.constructor = Son;
+Son.prototype.sayAge = function() {
+  console.log(this.age);
+};
+
+const instance1 = new Son("Tom", 24);
+instance1.sayName(); // 'Tom'
+instance1.sayAge(); // 24
+instance1.colors.push("pink");
+console.log(instance1.colors); // [ 'red', 'green', 'pink' ]
+
+const instance2 = new Son("Marry", 12);
+instance2.sayName(); // 'Marry'
+instance2.sayAge(); // 12
+instance2.colors.push("black");
 console.log(instance2.colors); // [ 'red', 'green', 'black' ]
-console.log(Father()); // [ 'red', 'green', 'black' ]
