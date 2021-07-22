c8e9f23cacc86bbf1f4d958c78eb98eda0fd5ba6
lixingjuan <18236129679@163.com>
2021-07-22 20:57:34 +0800

    add: 固定思维和成长性思维

--- del.js
+++ del.js
@@ -1,54 +1,48 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var lengthOfLongestSubstring = function (s) {
-  const string = s ?? '';
+// const book = {
+//   title: 'Profeeeional javas',
+//   authors: ['2', 2],
+//   edition: 4444,
+//   year: 2018
+// }
 
-  if (string.length <= 1) {
-    return string.length
-  }
+let book = {
+  title: "Professional",
+  authors: [
+    "Nicholas C. Zakas",
+    "Matt Frisbie"
+  ],
+  edition: 4,
 
-  const arrSring = Array.from(string)
-
-  let maxLength = 0;
-
-  let arr = [];
-
-
-  for (let index = 0; index < arrSring.length; index++) {
-    const it = arrSring[index];
-    if (!arr.length) {
-      arr.push(it)
-      maxLength = 1
-    } else {
-      // 最后出现的索引
-      const lastIndex = arr.lastIndexOf(it)
-
-      // 1. 如果没有出现过
-      if (lastIndex < 0) {
-        arr.push(it);
-        maxLength = Math.max(arr.length, maxLength)
-      } else {
-        // 2. 出现过，则将其最后出现的位置之前及其本身都删除
-        arr.splice(0, lastIndex + 1)
-        arr.push(it)
-        maxLength = Math.max(arr.length, maxLength)
-      }
-    }
-  }
-
-
-  return maxLength
 };
 
+let jsontext1 = JSON.stringify(book, (key, value) => {
+  switch (key) {
+    case "title":
+      return value.split('').join('-')
+    case "authors":
+      return value.join(",")
+    default:
+      return value
+  }
+})
 
-// console.time('hhhh')
-console.log(lengthOfLongestSubstring("abcabcbb")) // 3
-console.log(lengthOfLongestSubstring("bbbbb"))  // 1
-console.log(lengthOfLongestSubstring("pwwkew")) // 3
-console.log(lengthOfLongestSubstring("  "))   // 2
-console.log(lengthOfLongestSubstring("cbb"))    // 2
-console.log(lengthOfLongestSubstring("aab"))    // 2
-console.log(lengthOfLongestSubstring("dvdf"))   // 3
-// console.timeEnd('hhhh')
+
+
+console.log(jsontext1)
+
+
+
+let jsonTex2 = JSON.stringify(book, (key, value) => {
+  switch (key) {
+    case "authors":
+      return value.join(",")
+    case "year":
+      return 5000;
+    case "edition":
+      return undefined;
+    default:
+      return value;
+  }
+});
+
+console.log(jsonTex2)
