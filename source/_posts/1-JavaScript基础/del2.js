0b4d65c599e5bc1d0aac498aad5db4a983e2e5b0
lixingjuan <18236129679@163.com>
2021-08-26 07:05:36 +0800

    add: 添加异步函数

--- source/_posts/1-JavaScript基础/del2.js
+++ source/_posts/1-JavaScript基础/del2.js
@@ -1,1 +1,15 @@
-// Promise.all();
+async function a() {
+  console.log(await Promise.resolve(1));
+}
+
+async function b() {
+  console.log(await 2);
+}
+
+async function c() {
+  console.log(3);
+}
+
+a();
+b();
+c();
