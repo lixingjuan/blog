e111af69ee4fc1e76fb02b1d2fe4fa34e1a2b683
lixingjuan <xingjuan.li@datayes.com>
2022-01-16 13:04:44 +0800

    add: 1-IIFE解决作用域问题

---
+++ app.js
@@ -0,0 +1,13 @@
+(function name(params) {
+  var myName = "lixingjuan";
+  console.log(1);
+})();
+
+// console.log(myName);
+
+const result = (function name(params) {
+  var myName = "lixingjuan";
+  return myName;
+})();
+
+console.log(result);
