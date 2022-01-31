0314523bb7e16201dd87ed4e6fa56c5c388dd46f
lixingjuan <18236129679@163.com>
2022-01-31 15:37:27 +0800

    feat: part1-6 各种loader & 加载压缩css &  加载数据 & 加载自定义json文件 & 加载字体

---
+++ 06-manage-assets/webpack.config.js
@@ -0,0 +1,106 @@
+const path = require("path");
+// 构造函数或者类，大写命名
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const toml = require("toml");
+const yaml = require("yaml");
+const json5 = require("json5");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+    assetModuleFilename: "images/[contenthash][ext]",
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      filename: "app.html",
+      inject: "body",
+    }),
+
+    new MiniCssExtractPlugin({
+      // filename: "styles/[contenthash].css", // !! 自定义生成的css文件位置/名称
+    }),
+  ],
+
+  // 热更新
+  devServer: {
+    static: "./dist",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|less)$/,
+        /**
+         * 1. 此处支持字符串/数组
+         * 2. 顺序从后向前
+         * 3. !! 先使用css-loader使得打包没有问题，可识别css文件
+         *    再使用style-loader将css放置页面上
+         */
+        // use: ["style-loader", "css-loader", "less-loader"],
+
+        /**
+         * !! 使用 MiniCssExtractPlugin 中的一个loader插件替代 style-loader
+         * 此时执行 npx webpack 对项目打包后，再去查看dist文件夹，会发现
+         *  1. !! 多了main.css文件
+         *  2. app.html 引用该文件（HtmlWebpackPlugin的功劳）
+         */
+        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+      },
+
+      {
+        test: /\.(csv|tsv)$/,
+        use: "csv-loader",
+      },
+
+      {
+        test: /\.xml$/,
+        use: "xml-loader",
+      },
+
+      {
+        test: /\.toml$/,
+        type: "json",
+        parser: {
+          parse: toml.parse,
+        },
+      },
+
+      {
+        test: /\.yaml$/,
+        type: "json",
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+
+      {
+        test: /\.json5$/,
+        type: "json",
+        parser: {
+          parse: json5.parse,
+        },
+      },
+
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      new CssMinimizerWebpackPlugin({
+        // filename: "styles/[contenthash].css", // !! 自定义生成的css文件位置/名称
+      }),
+    ],
+  },
+};
