3059e2958f5b338fb85f668609fdde833ae8c252
lixingjuan <42627664+lixingjuan@users.noreply.github.com>
2022-05-16 11:17:01 +0800

    fix: 删除多余文件&增加Promise.all

---
+++ docs/手写类题目/code/PromiseAll.js
@@ -0,0 +1,35 @@
+const PromiseAll = (arr) => {
+  const fns = arr.map((it) => Promise.resolve(it));
+  const result = [];
+  let counter = 0;
+
+  return new Promise((resolve, reject) => {
+    fns.forEach((fn, index) => {
+      fn.then((res) => {
+        // !! 由于单个Promise返回的顺序不同，
+        // 所以赋值给 result[index]，
+        // 并采用counter 计数的方式，判断当前Promise数组是否全部完成
+        result[index] = res;
+        if (++counter === arr.length) {
+          resolve(result);
+        }
+      }).catch(reject);
+    });
+  });
+};
+
+/* ****************************************************************************************************
+ *                                    tests
+ ************************************************************************************************* */
+
+const test1 = () => {
+  PromiseAll([1, Promise.resolve(2)]).then(console.log);
+};
+
+const test2 = () => {
+  PromiseAll([1, Promise.reject(2)])
+    .then(console.log)
+    .catch(console.log);
+};
+
+module.exports = { PromiseAll, test1, test2 };
