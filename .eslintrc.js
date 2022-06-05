57537f6e650a15c29b699aa0515ccffdb811616c
lixingjuan <42627664+lixingjuan@users.noreply.github.com>
2022-06-05 18:37:20 +0800

    add: 搭建本地react debug环境

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,26 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: "eslint:recommended",
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "spaced-comment": 1,
-    'no-prototype-builtins': 0,
+    "no-prototype-builtins": 0,
     "no-unused-vars": 1,
-  }
+  },
+  eslintIgnore: [
+    "React/debug-react/react.development.js",
+    "React/debug-react/react-dom.development.js",
+  ],
 };
