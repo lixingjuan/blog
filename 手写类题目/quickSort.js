fea1f1056a5872cf73c3f8c6e18f0cff1012b40b
lixingjuan <42627664+lixingjuan@users.noreply.github.com>
2022-06-06 00:59:48 +0800

    update: 更新quickSort

--- 手写类题目/quickSort.js
+++ 手写类题目/quickSort.js
@@ -1,3 +1,12 @@
+/**
+ * @name 快速排序
+ * @description 将数组分成两半，递归进行排序，选择基准值，循环和基准值做比较，大于基准值的放在右边，否砸放在左边
+ *
+ * 平均时间复杂度O(nlogn)
+ * 最优时间复杂度O(nlogn)
+ * 最坏时间复杂度O(n^2)
+ * 空间复杂度和时间有关
+ */
 const sortArray = (nums) => {
   const partion = (arr, low, high) => {
     const pivot = arr[low];
@@ -19,12 +28,14 @@ const sortArray = (nums) => {
     // 最后将pivot放在正确的位置
     arr[low] = pivot;
 
+    // 此时low === high, 返回其索引，便于将数组拆分
     return high;
   };
 
   const quickSort = (arr, left, right) => {
     if (left < right) {
       const pivot = partion(arr, left, right);
+      // 拆分的数组分别排序
       quickSort(arr, left, pivot - 1);
       quickSort(arr, pivot + 1, right);
     }
