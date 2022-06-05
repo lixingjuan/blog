0d8b189041d0cc3965c2eff500870eda5b53cfa7
lixingjuan <42627664+lixingjuan@users.noreply.github.com>
2022-06-05 17:10:13 +0800

    update: quickSort

--- 手写类题目/quickSort.js
+++ 手写类题目/quickSort.js
@@ -1,48 +1,37 @@
-const quickSort = (array) => {
-  const sort = (arr, lowIndex = 0, highIndex = arr.length - 1) => {
-    // 如果左边的索引大于等于右边的索引说明整理完毕
-    if (arr.length <= 1 || lowIndex >= highIndex) {
-      return arr;
-    }
+const sortArray = (nums) => {
+  const partion = (arr, low, high) => {
+    const pivot = arr[low];
 
-    let low = lowIndex;
-    let high = highIndex;
-
-    // core-start
     while (low < high) {
-      while (low < high && arr[low] <= arr[high]) {
-        low++;
-      }
-      [arr[high], arr[low]] = [arr[low], arr[high]];
-
-      while (low < high && arr[low] <= arr[high]) {
+      // 先从右边开始找到比pivot小的数，赋值该数给low
+      while (low < high && arr[high] >= pivot) {
         high--;
       }
-      [arr[high], arr[low]] = [arr[low], arr[high]];
+      arr[low] = arr[high];
+
+      // 再从左边开始找到比pivot大的数，赋值该数给high
+      while (low < high && arr[low] <= pivot) {
+        low++;
+      }
+      arr[high] = arr[low];
     }
-    // core-end
 
-    // core-start
-    // 将左边的无序数组, 重复上面的操作
-    sort(arr, lowIndex, high - 1);
-    // 将右边的无序数组, 重复上面的操作
-    sort(arr, high + 1, highIndex);
-    // core-end
+    // 最后将pivot放在正确的位置
+    arr[low] = pivot;
 
-    return arr;
+    return high;
   };
 
-  return sort(array);
+  const quickSort = (arr, left, right) => {
+    if (left < right) {
+      const pivot = partion(arr, left, right);
+      quickSort(arr, left, pivot - 1);
+      quickSort(arr, pivot + 1, right);
+    }
+  };
+
+  quickSort(nums, 0, nums.length - 1);
+  return nums;
 };
 
-console.log(quickSort([]));
-console.log(quickSort([1, 2]));
-console.log(quickSort([1]));
-console.log(
-  quickSort([3, 1, 44, 38, 5, 47, 15, 88, 23, 21, 12, 333, 218, 36, 26, 27, 2, 46, 4, 19, 50, 48])
-);
-console.log(
-  quickSort([
-    1, 90, 3, 44, 38, 5, 47, 15, 88, 23, 21, 12, 333, 218, 36, 26, 27, 2, 46, 4, 19, 50, 48,
-  ])
-);
+console.log(sortArray([3, 2, 1]));
