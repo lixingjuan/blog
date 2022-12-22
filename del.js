c2f575b269e92522a39e9d8148c141406fcbc8d5
lixingjuan <42627664+lixingjuan@users.noreply.github.com>
2022-12-22 14:33:10 +0800

    update: 无脑更新

--- del.js
+++ del.js
@@ -1,22 +1,36 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @des 中心扩散法
+ * 2. 定义两个变量，一个start存储最大回文字符串的开始位置，maxLength 记录最大长度，则返回结果  s.subString(start, start+maxLength)
+ * 3. 创建一个helper 函数，入参 左右指针，判断left 和right 位置的值是否相等，left right指针是否越界，
+ * 如果以上条件都满足，则判断是否要更回文字符串最大长度和起始字符串的位置，然后left--, right++, 直至不满足条件为止‘
+ * 4. 遍历字符串，每个位置调用 helper 函数两遍，第一遍检查 i-1, i+1, 第二遍检查 i, i+
+ * @param {string} s
+ * @return {string}
  */
-/**
- * @param {TreeNode} root
- * @param {number} val
- * @return {TreeNode}
- */
-var searchBST = function (root, val) {
-  while (root) {
-    if (root.val === val) {
-      return root;
-    }
-    root = val < root.val ? root.left : root.right;
+const longestPalindrome = (s = "") => {
+  if (s.length < 2) {
+    return s.slice(0, 1);
   }
-  return null;
+  let start = 0;
+  let maxLength = 1;
+  const strArr = [...s];
+
+  const expandAroundCenter = (left, right) => {
+    // left 和 right 指针都在数组范围内才处理
+    while (left >= 0 && right < s.length && strArr[left] === strArr[right]) {
+      if (right - left + 1 > maxLength) {
+        maxLength = right - left + 1;
+        start = left;
+      }
+      left--;
+      right++;
+    }
+  };
+
+  for (let i = 0; i < s.length; i += 1) {
+    expandAroundCenter(i - 1, i + 1);
+    // 当 i=2
+    expandAroundCenter(i, i + 1);
+  }
+  return s.substring(start, start + maxLength);
 };
