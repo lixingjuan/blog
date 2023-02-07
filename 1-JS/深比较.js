af561d044272254c9273ce6c47fe40dd96d41afa
lixingjuan <42627664+lixingjuan@users.noreply.github.com>
2023-02-07 22:09:39 +0800

    update: 深比较

--- 1-JS/深比较.js
+++ 1-JS/深比较.js
@@ -3,18 +3,22 @@ const isObject = (val) => {
 };
 
 const deepCompare = (objA, objB) => {
+  // 如果都不是 object类型，直接 === 比较是否相等
   if (!isObject(objA) && !isObject(objB)) {
     return objA === objB;
   }
 
+  // 如果 === 为true, 则直接返回true
   if (objA === objB) {
     return true;
   }
 
+  // 如果长度不相等，则直接返回false
   if (Object.keys(objA).length !== Object.keys(objB).length) {
     return false;
   }
 
+  // 循环，一旦不相等，立即return
   for (let key in objA) {
     const res = deepCompare(objA[key], objB[key]);
 
