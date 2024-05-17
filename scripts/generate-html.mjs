214f8dedb05fc5b653c9723d65af0ed168d29548
lixingjuan <18236129679@163.com>
2024-05-17 18:01:06 +0800

    test

--- scripts/generate-html.mjs
+++ scripts/generate-html.mjs
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const marked = require('marked');
+import fs from 'fs';
+import path from 'path';
+import { marked } from 'marked';
 
 // 递归遍历目录
 function walkDir(dir, callback) {
